73b41844588eaac2f95b6c1bf7c866e74420b60d
Fabian Fagerholm <fabbe@paniq.net>
2016-01-07 20:26:10 +0200

    Remove links between ideas in the model

--- ideas/models.py
+++ ideas/models.py
@@ -4,7 +4,6 @@ class Idea(models.Model):
     title = models.CharField(max_length=200)
     created = models.DateTimeField()
     text = models.TextField()
-    related = models.ManyToManyField('self')
     
     def __str__(self):
         return self.title
